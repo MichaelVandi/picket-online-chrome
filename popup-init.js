ca33f74003e98e50fcf4503bc3787225622e23d2
Michael Vandi <mikevee2013@gmail.com>
2023-02-18 15:46:34 -0800

    Added propose a boycott screen

--- popup-init.js
+++
@@ -1,9 +0,0 @@
-// function init() {
-//     chrome.runtime.sendMessage({ message: "is-user-signed-in" }, function (response) {
-//         if (response === "success") {
-//             window.location.replace("./main.html")
-//         } 
-//     });
-// }
-
-// init();
