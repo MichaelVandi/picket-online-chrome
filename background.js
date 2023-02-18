6989950602a6a4f492694219118e67e0a14209d7
Michael Vandi <mikevee2013@gmail.com>
2023-02-18 13:24:08 -0800

    first commit

---
+++ background.js
@@ -0,0 +1,37 @@
+let userSignedIn = false;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    switch (request.message) {
+        case "is-user-signed-in":
+            sendResponse({
+                message: 'success',
+                payload: userSignedIn
+            });
+            break;
+        case "sign-out":
+            userSignedIn = false;
+            sendResponse({ message: "success" });
+            break;
+        case "sign-in":
+            userSignedIn = true;
+            sendResponse({ message: "success" });
+            break;
+        case "get-auth-token":
+            // chrome.identity.getAuthToken({interactive: true}, function(token) {
+            //     sendResponse({
+            //         message: "success",
+            //         token: token,
+            //     });
+            // });
+            break;
+        case "open-options":
+            chrome.runtime.openOptionsPage();
+            break;
+        default:
+            chrome.action.setPopup({popup: "./login.html"}, () => {
+                sendResponse({message: "success"});
+            });
+            break;
+    }
+    return true;
+})
---
+++ tooltip.js
@@ -0,0 +1,74 @@
+
+let picketVisible = false;
+
+window.onload = async function() {
+    // Should you init a picket line here?
+    // Make an API call, if true then init
+    const checkBoycott = await checkBoycottStatus("test");
+    
+    if (checkBoycott.boycott) {
+        showPicketLine(checkBoycott.data);
+    }
+}
+
+async function checkBoycottStatus(domain) {
+    return {
+        "boycott": true,
+        "data": {
+            "header": "Don't cross the picket line",
+            "numUsers": "3.4m",
+            "site": "YouTube", 
+        },
+    }
+}
+
+function showPicketLine(data) {
+    if (picketVisible) return; // Don't show picket if it is already visible
+    const picketLine = initPicketLineUI(data);
+    // Get body and append
+    console.log("showing picket line")
+    document.body.append(picketLine);
+    picketVisible = true;
+}
+
+function initPicketLineUI(data) {
+    // Create UI elements and configure
+    const container = document.createElement("div");
+    container.setAttribute("class", "picket-container");
+    
+    const heading = document.createElement("p");
+    heading.setAttribute("class", "picket-heading");
+    heading.innerHTML = data.header;
+
+    const img = document.createElement("img");
+    img.setAttribute("class", "picket-cover");
+    img.setAttribute("src", "https://i.imgur.com/pG5GFIw.png");
+    img.setAttribute("alt", "picket cover");
+
+    const proposalCount = document.createElement("p");
+    proposalCount.setAttribute("class", "proposal-count");
+    proposalCount.innerHTML = data.numUsers + " users currently boycotting";
+
+    const proposalTitle = document.createElement("p");
+    proposalTitle.setAttribute("class", "proposal-title");
+    proposalTitle.innerHTML = "We are currently in negotiations with " + data.site;
+
+    const learnMoreButton = document.createElement("div");
+    learnMoreButton.setAttribute("class", "primary-button");
+    learnMoreButton.innerHTML = "Learn more";
+
+    const skipButton = document.createElement("p");
+    skipButton.setAttribute("class", "cross-picket-button");
+    skipButton.innerHTML = "Cross it.";
+
+    // Append all to container
+    container.append(heading);
+    container.append(img);
+    container.append(proposalCount);
+    container.append(proposalTitle);
+    container.append(learnMoreButton);
+    container.append(skipButton);
+
+    return container;
+}
+
